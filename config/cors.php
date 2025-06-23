4097b80420551dc65cd447ca06262bc4b3eecf44
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2025-06-23 20:45:59 +0300

    refactoring

--- config/cors.php
+++ config/cors.php
@@ -8,6 +8,7 @@ return [
         'http://localhost:5173',
         'http://localhost:5174',
         'http://localhost:5176',
+        'https://ekbcomplex.ru'
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
