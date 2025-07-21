e5a38fd0424d7fa2d68755d60e99ff3ebc3c563d
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2025-07-21 12:29:30 +0300

    job table update

--- app/Models/ImportStatus.php
+++ app/Models/ImportStatus.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Str;
 class ImportStatus extends Model
 {
     protected $fillable = ['file_name', 'status', 'message'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->uuid)) {
+                $model->uuid = (string) Str::uuid();
+            }
+        });
+    }
 }
