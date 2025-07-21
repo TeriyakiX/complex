60c24e82e3bb1508e83fb373ac55268d835c43c6
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2025-07-21 14:22:43 +0300

    filter

--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -19,7 +19,9 @@ class Manufacturer extends Model
 
     public function scopeWithImagePriorityAndSortedName($query)
     {
-        return $query->orderByRaw("CASE WHEN image IS NOT NULL AND image != '' THEN 0 ELSE 1 END")
+        return $query
+            ->orderByRaw('CASE WHEN products_count > 0 THEN 0 ELSE 1 END')
+            ->orderByRaw("CASE WHEN image IS NOT NULL AND image != '' THEN 0 ELSE 1 END")
             ->orderByRaw("CASE WHEN LEFT(name, 1) REGEXP '^[0-9]' THEN 1 ELSE 0 END")
             ->orderBy('name');
     }
