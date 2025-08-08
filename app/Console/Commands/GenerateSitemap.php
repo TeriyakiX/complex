9eab71808dbfd38801943855d77d45f76fee9919
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2025-08-08 16:35:43 +0300

    sitemaps

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -18,7 +18,7 @@ class GenerateSitemap extends Command
 
     public function handle()
     {
-        $baseUrl = 'https://ekbcomplex.ru';
+        $baseUrl = 'https://api.ekbcomplex.ru';
         $basePath = public_path('sitemaps');
 
         if (!is_dir($basePath)) {
